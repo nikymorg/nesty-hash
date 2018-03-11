8fea1b61e6bc6afcb4beff6d757c66ac6ea33375
nikymorg <nicole.morgan@gmail.com>
2018-03-11 18:51:23 -0400

    add ace props

--- src/components/snippet.js
+++ src/components/snippet.js
@@ -10,37 +10,21 @@ import 'brace/mode/json'
 import 'brace/mode/python'
 import 'brace/mode/markdown'
 
-const Snippet = ({ content }) => {
-  return (
-    <AceEditor
-      wrapEnabled
-      width='80%'
-      mode='html_ruby'
-      theme='solarized_dark'
-      fontSize={16}
-      name='snippet'
-      readOnly={true}
-      editorProps={{
-        $blockScrolling: true
-      }}
-      value={JSON.stringify(content)}
-    />
-  )
-}
+const Snippet = ({ content }) => (
+  <AceEditor
+    wrapEnabled
+    width='80%'
+    mode='html_ruby'
+    theme='solarized_dark'
+    fontSize={16}
+    name='snippet'
+    readOnly={true}
+    editorProps={{
+      $blockScrolling: true
+    }}
+    value={JSON.stringify(content)}
+  />
+)
+
 
 export default Snippet
-
-
-// <AceEditor
-// wrapEnabled
-// height='100%'
-// width='100%'
-// theme='github'
-// mode='html_ruby'
-// fontSize={14}
-// value={'me is stuff'}
-// readOnly={true}
-// setOptions={{
-//   tabSize: 2
-// }}
-// />
