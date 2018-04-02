6e16afc18bd6d9769e593e95134ccda9a7c7b8d2
nikymorg <nicole.morgan@gmail.com>
2018-04-02 19:38:28 -0400

    gradient padding

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ import { Header } from 'semantic-ui-react'
 
 const AppHeader = ({ language }) => {
   return (
-    <div className={`${language}-gradient item-header header-container`}>
+    <div className={`${language}-gradient item-header header-container padding-top-l`}>
       <Header as='h1' className='title'>Nesty Hash</Header>
       <Header as='h3' className='subtitle'>Randomly generate nested hashes to practice your coding skills</Header>
     </div>
